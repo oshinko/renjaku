c720498ef1eb48db8a3b4ff1d4aa8a05c21d9fb3
Oshinko <osnk@renjaku.jp>
2020-08-02 12:46:51 +0900

    Update dev server of www.renjaku.co.jp

--- www.renjaku.co.jp.py
+++ www.renjaku.co.jp.py
@@ -1,5 +1,6 @@
 import pathlib
 import sys
+import webbrowser
 import wsgiref.simple_server
 
 workspace = 'www.renjaku.co.jp'
@@ -22,6 +23,7 @@ if __name__ == '__main__':
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
     httpd = wsgiref.simple_server.make_server('', port, app)
     print(f'Serving {workspace} on port {port}, control-C to stop')
+    webbrowser.open(f'http://localhost:{port}')
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
